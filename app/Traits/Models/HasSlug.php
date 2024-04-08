83379f22f5e32fc1fe0a78c8c6e7e740aa4d5221
paveld <pavel.termhg@gmail.com>
2024-04-08 19:09:00 +0300

    fix(hasSlug):add method for generating uniqueSlugs

--- app/Traits/Models/HasSlug.php
+++ app/Traits/Models/HasSlug.php
@@ -4,18 +4,30 @@ namespace App\Traits\Models;
 
 use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 trait HasSlug
 {
     protected static function bootHasSlug(): void
     {
-
-        static::creating(function (Model $item) {
-            $item->slug = $item->slug ?? str($item::slugFrom())
-                                            //TODO implement function for checking if slug already exists
-                                            ->append(time() + rand(1,2000))
-                                            ->slug();
+        static::creating(function(Model $item){
+            if (!isset($item->slug)){
+                $newSlug = str($item->{static::slugFrom()})->slug();
+                $item->slug = self::createUniqueSlugFrom($newSlug);
+            }
         });
+    }
+    public static function createUniqueSlugFrom($newSlug){
+        if (self::where("slug", (string)$newSlug)->exists()) {
+            $check = preg_match('/([0-9]+)/', (string)$newSlug, $match);
+            if(!$check){
+                return self::createUniqueSlugFrom($newSlug . "_1");
+            }
+            $newSlug = preg_replace('/([0-9]+)$/',$match[1] + 1,$newSlug);
+            return self::createUniqueSlugFrom($newSlug);
+        }else{
+            return $newSlug;
+        }
     }
     public static function slugFrom(): string{
         return 'title';
